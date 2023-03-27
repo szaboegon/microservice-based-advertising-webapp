1256a2148f428c70ee7de279ce293542aca5f00b
szaboegon <szabo.egon2001@gmail.com>
2023-03-27 20:32:18 +0200

    Identity service base

---
+++ StudentHousing/IdentityService/Controllers/IdentityController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IdentityController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        public IdentityController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> LoginAsync()
+        {
+            return Ok();
+        }
+
+    }
+}
---
+++ StudentHousing/IdentityService/Models/LoginData.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace IdentityService.Models
+{
+    public class LoginData
+    {
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+    }
+}
